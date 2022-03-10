f8d3a333e6925d2b4b0c1bb6d930fd541e5b50b0
Andrew McCauley <uxmccauley@gmail.com>
2022-03-10 06:59:42 -0600

    fixing vercel errors "location not found"

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -6,51 +6,50 @@ import {useRouter} from "next/router";
 import {useCookie} from 'next-cookie'
 
 export default function SignIn() {
-    const cookie = useCookie()
+    // const cookie = useCookie()
     const router = useRouter()
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
-    // console.log(window.location)
 
-    const checkUser = async () => {
-        await fetch('/api/users', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({username: username, password: password})
-        })
-            .then(response => response.json())
-            .then(data => {
-                setError(false)
-                setIsLoading(true)
-                cookie.set("REIGN_username", data.username, {
-                    domain: "localhost",
-                    expires: 1,
-                })
-                cookie.set("REIGN_firstName", data.firstName, {
-                    domain: "localhost",
-                    expires: 1,
-                })
-                cookie.set("REIGN_lastName", data.lastName, {
-                    domain: "localhost",
-                    expires: 1,
-                })
-                cookie.set("REIGN_loggedIn", true, {
-                    domain: "localhost",
-                    expires: 1,
-                })
-                setTimeout(() => {
-                    router.push("/dashboards/executive-summary", null, { shallow: true })
-                }, 5000)
-            })
-            .catch(e => {
-                setError(true)
-            })
-
-    }
+    // const checkUser = async () => {
+    //     await fetch('/api/users', {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({username: username, password: password})
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             setError(false)
+    //             setIsLoading(true)
+    //             cookie.set("REIGN_username", data.username, {
+    //                 domain: "localhost",
+    //                 expires: 1,
+    //             })
+    //             cookie.set("REIGN_firstName", data.firstName, {
+    //                 domain: "localhost",
+    //                 expires: 1,
+    //             })
+    //             cookie.set("REIGN_lastName", data.lastName, {
+    //                 domain: "localhost",
+    //                 expires: 1,
+    //             })
+    //             cookie.set("REIGN_loggedIn", true, {
+    //                 domain: "localhost",
+    //                 expires: 1,
+    //             })
+    //             setTimeout(() => {
+    //                 router.push("/dashboards/executive-summary", null, { shallow: true })
+    //             }, 5000)
+    //         })
+    //         .catch(e => {
+    //             setError(true)
+    //         })
+    //
+    // }
 
     return (
         <>
@@ -68,7 +67,7 @@ export default function SignIn() {
                     <h1>Sign In</h1>
                     <span className={`${styles.error} ${error === true ? styles.active : null}`}>Username or password incorrect. Please try again.</span>
                     <form onSubmit={() => {
-                        router.push("/dashboards/executive-summary", null, { shallow: true })
+                        router.push("/dashboards/executive-summary")
                     }}>
                         <input type={"text"} value={username} autoComplete={"current-username"} onChange={(event) => {
                             setUsername(event.target.value)
