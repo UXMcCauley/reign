243fe92f9adf1625c206ac63228989e3a3423bc0
Andrew McCauley <uxmccauley@gmail.com>
2022-03-10 14:23:21 -0600

    add the add shifts functionality to schedules page and single page

--- pages/schedules/add-schedules.js
+++ pages/schedules/add-schedules.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import ContentContainer from "../../components/universal/layout"
+import AddShiftsFlyin from "https://framer.com/m/AddShiftsFlyin-geLH.js@02o8EOYS6J0lZml4aRu3"
+
 
 export default function AddSchedules() {
     return (
@@ -9,6 +11,7 @@ export default function AddSchedules() {
                 <meta name="description" content="Add Schedules"/>
             </Head>
             <ContentContainer>
+                <AddShiftsFlyin style={{backgroundColor: "transparent"}} variant={"Variant 2"}/>
             </ContentContainer>
         </>
     )
--- pages/schedules/schedule-overview.js
+++ pages/schedules/schedule-overview.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import ContentContainer from "../../components/universal/layout"
 import SchedulingAll from "https://framer.com/m/SchedulingAll-Dcuy.js@zfxJqulnWLCj6Qia4N97"
+import AddShiftsFlyin from "https://framer.com/m/AddShiftsFlyin-geLH.js@8en6jtfD1VBzk84iOZlS"
+import styles from "./styles/ScheduleOverview.module.scss"
+import {useState} from "react";
 
-export default function Home() {
+export default function ScheduleOverview() {
+    const [open, setOpen] = useState(false)
     return (
         <>
             <Head>
@@ -10,19 +14,26 @@ export default function Home() {
                 <meta name="description" content="Schedules"/>
             </Head>
             <ContentContainer>
-                <SchedulingAll onClick={() => {
-                    alert("Clicked")
-                }}
-                               onClickDay={(event) => {
-                                   console.log(event)
-                               }}
-                               onClickWeek={() => {
-                                   alert("week")
-                               }}
-                               onClickMonth={() => {
-                                   alert("month")
-                               }}
-                />
+                <div className={styles.container}>
+                    <SchedulingAll onClick={() => {
+                        setOpen(true)
+                    }}
+                                   onClickDay={(event) => {
+                                       console.log(event)
+                                   }}
+                                   onClickWeek={() => {
+                                       alert("week")
+                                   }}
+                                   onClickMonth={() => {
+                                       alert("month")
+                                   }}
+                    />
+                    <div className={`${styles.flyin} ${open === true ? styles.open : null}`}>
+                        <AddShiftsFlyin saveShifts={() => {setOpen(false)}}
+                            closeOverlay={() => {setOpen(false)}}/>
+                    </div>
+                </div>
+
             </ContentContainer>
         </>
     )
