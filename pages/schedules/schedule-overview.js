ed07f72158a61e423f2aca8f6291a3eb01740ed1
Andrew McCauley <uxmccauley@gmail.com>
2022-03-14 14:24:38 -0500

    removed framer component to make vercel happy

--- pages/schedules/schedule-overview.js
+++ pages/schedules/schedule-overview.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import ContentContainer from "../../components/universal/layout"
-import SchedulingAll from "https://framer.com/m/SchedulingAll-Dcuy.js@zfxJqulnWLCj6Qia4N97"
-import AddShiftsFlyin from "https://framer.com/m/AddShiftsFlyin-geLH.js@8en6jtfD1VBzk84iOZlS"
+// import SchedulingAll from "https://framer.com/m/SchedulingAll-Dcuy.js@zfxJqulnWLCj6Qia4N97"
+// import AddShiftsFlyin from "https://framer.com/m/AddShiftsFlyin-geLH.js@8en6jtfD1VBzk84iOZlS"
 import styles from "./styles/ScheduleOverview.module.scss"
 import {useState} from "react";
 
@@ -15,22 +15,22 @@ export default function ScheduleOverview() {
             </Head>
             <ContentContainer>
                 <div className={styles.container}>
-                    <SchedulingAll onClick={() => {
-                        setOpen(true)
-                    }}
-                                   onClickDay={(event) => {
-                                       console.log(event)
-                                   }}
-                                   onClickWeek={() => {
-                                       alert("week")
-                                   }}
-                                   onClickMonth={() => {
-                                       alert("month")
-                                   }}
-                    />
+                    {/*<SchedulingAll onClick={() => {*/}
+                    {/*    setOpen(true)*/}
+                    {/*}}*/}
+                    {/*               onClickDay={(event) => {*/}
+                    {/*                   console.log(event)*/}
+                    {/*               }}*/}
+                    {/*               onClickWeek={() => {*/}
+                    {/*                   alert("week")*/}
+                    {/*               }}*/}
+                    {/*               onClickMonth={() => {*/}
+                    {/*                   alert("month")*/}
+                    {/*               }}*/}
+                    {/*/>*/}
                     <div className={`${styles.flyin} ${open === true ? styles.open : null}`}>
-                        <AddShiftsFlyin saveShifts={() => {setOpen(false)}}
-                            closeOverlay={() => {setOpen(false)}}/>
+                        {/*<AddShiftsFlyin saveShifts={() => {setOpen(false)}}*/}
+                        {/*    closeOverlay={() => {setOpen(false)}}/>*/}
                     </div>
                 </div>
 
