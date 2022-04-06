3beb91f25311d2baadec6b61f8f251e1688458a9
Andrew McCauley <uxmccauley@gmail.com>
2022-04-06 05:48:48 -0500

    Inequity finder with table 4

--- pages/employees/components/EmployeeCompareLine.js
+++ pages/employees/components/EmployeeCompareLine.js
@@ -1,7 +1,7 @@
 function EmployeeCompareLine({selectedEmployees, line}) {
 
     let symbol, color
-    if(selectedEmployees !== undefined && selectedEmployees.length > 0) {
+    if(selectedEmployees !== undefined ) {
         if (parseFloat(selectedEmployees[0][line]) > parseFloat(selectedEmployees[1][line])) {
             symbol = ">"
             color = "bg-pink-700"
@@ -19,7 +19,7 @@ function EmployeeCompareLine({selectedEmployees, line}) {
             <div className={`flex`}>
                 <div className={`flex w-2/5 p-2 align-middle justify-center ${line === "name" ? "border border-4 border-pink-700 rounded-2xl" : null}`}>
                     <div
-                        className={"text-black self-center dark:text-white"}>{selectedEmployees.length === 2 ? selectedEmployees[0][line] : null}</div>
+                        className={"text-black self-center dark:text-white"}>{selectedEmployees && selectedEmployees.length === 2 ? selectedEmployees[0][line] : null}</div>
                 </div>
                 <div className={`flex w-1/5 p-2 align-middle justify-center text-center `}>
                     <div
@@ -27,7 +27,7 @@ function EmployeeCompareLine({selectedEmployees, line}) {
                 </div>
                 <div className={`flex w-2/5 p-2 text-center align-middle justify-center  ${line === "name" ? "border border-4 border-violet-700 rounded-2xl" : null}`}>
                     <div
-                        className={"text-black self-center dark:text-white"}>{selectedEmployees.length === 2 ? selectedEmployees[1][line] : null}</div>
+                        className={"text-black self-center dark:text-white"}>{selectedEmployees && selectedEmployees.length === 2 ? selectedEmployees[1][line] : null}</div>
                 </div>
             </div>
         </div>
