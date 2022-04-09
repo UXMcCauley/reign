9434fc7c9864165dae4a108f221f9ea1e94a08a8
Andrew McCauley <uxmccauley@gmail.com>
2022-04-09 08:12:16 -0500

    test commit

--- components/TabbedNavigation.js
+++ components/TabbedNavigation.js
@@ -1,11 +1,12 @@
 import {useRouter} from "next/router"
+import Link from "next/link"
 
 const tabs = [
-    { name: 'Executive Summary', href: '/dashboards/executive-summary', current: true },
-    { name: 'Employee Performance', href: '/dashboards/employee-performance', current: false },
-    { name: 'Workforce Diversity', href: '/dashboards/workforce-diversity', current: false },
-    { name: 'Salary Overview', href: '/dashboards/salary-overview', current: false },
-    { name: 'Project Dynamics', href: '/dashboards/project-dynamics', current: false },
+    {name: 'Executive Summary', href: '/dashboards/executive-summary', current: true},
+    {name: 'Employee Performance', href: '/dashboards/employee-performance', current: false},
+    {name: 'Workforce Diversity', href: '/dashboards/workforce-diversity', current: false},
+    {name: 'Salary Overview', href: '/dashboards/salary-overview', current: false},
+    {name: 'Project Dynamics', href: '/dashboards/project-dynamics', current: false},
 ]
 
 function classNames(...classes) {
@@ -36,19 +37,18 @@ export default function TabbedNavigation() {
                 <div className="border-b border-gray-200 dark:border-gray-800">
                     <nav className="-mb-px flex space-x-8" aria-label="Tabs">
                         {tabs.map((tab) => (
-                            <a
-                                key={tab.name}
-                                href={tab.href}
-                                className={classNames(
+                            <Link key={tab.name} href={tab.href} passHref>
+                                <a className={classNames(
                                     router.pathname === tab.href
                                         ? 'border-violet-500 text-violet-600'
                                         : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
                                     'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
                                 )}
-                                aria-current={tab.current ? 'page' : undefined}
-                            >
-                                {tab.name}
-                            </a>
+                                   aria-current={tab.current ? 'page' : undefined}
+                                >
+                                    {tab.name}
+                                </a>
+                            </Link>
                         ))}
                     </nav>
                 </div>
